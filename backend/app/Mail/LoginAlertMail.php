dbd18952a9d0b35eb1f8ff5cf41662d6a0f2b486
AlioM46 <aliomaralsloom@gmail.com>
2026-01-18 03:29:23 +0300

    trying to fix mail service

--- backend/app/Mail/LoginAlertMail.php
+++ backend/app/Mail/LoginAlertMail.php
@@ -2,13 +2,12 @@
 
 namespace App\Mail;
 
-use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
 class LoginAlertMail extends Mailable
 {
-    use Queueable, SerializesModels;
+  use  SerializesModels;
 
     public function __construct(
         public string $name,
